b0de0874809bdbbcf88ebf912a45494b588496f5
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-11-27 10:56:35 +0700

    add function

--- encryt transpose/encryptTranspose.cpp
+++ encryt transpose/encryptTranspose.cpp
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-void transposeMethod(int n, int m, int** p, int** t);
+void transposeMethod(int , int , int** , int** );
 int main()
 {
 	int A, n, m, transpose;
